6a089ec46d25173a83419bc161998968f51a685d
Nic Raboy <nicolas.raboy@mongodb.com>
2021-05-11 14:02:51 -0700

    started adding realm. upgrade to unity alpha

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,13 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Realms;
+using Realms.Sync;
 
 public class GameController : MonoBehaviour {
+
+    public string RealmAppId;
 
     public float enemyTimer = 2.0f;
     public float timeUntilEnemy = 1.0f;
     public float minTimeUntilEnemy = 0.25f;
     public float maxTimeUntilEnemy = 2.0f;
+
+    private Realm _realm;
+    private User _realmUser;
+    private PlayerProfile _playerProfile;
+
+    async void OnEnable() {
+        // App realmApp = App.Create(RealmAppId);
+        // _realmUser = await realmApp.LogInAsync(Credentials.Anonymous());
+        _realmUser = await (App.Create(RealmAppId)).LogInAsync(Credentials.Anonymous());
+        Debug.Log("Logged in with user " + _realmUser.Id);
+        _realm = await Realm.GetInstanceAsync(new SyncConfiguration("game", _realmUser));
+        _playerProfile = _realm.Find<PlayerProfile>(_realmUser.Id);
+        if(_playerProfile == null) {
+            _realm.Write(() => {
+                _playerProfile = _realm.Add(new PlayerProfile(_realmUser.Id));
+            });
+        }
+    }
+
+    void OnDisable() {
+        if(_realm != null) {
+            _realm.Dispose();
+        }
+    }
 
     void Update() {
         timeUntilEnemy -= Time.deltaTime;
---
+++ Assets/Scripts/PlayerProfile.cs
@@ -0,0 +1,31 @@
+using Realms;
+using Realms.Sync;
+using MongoDB.Bson;
+
+public class PlayerProfile : RealmObject
+{
+
+    [PrimaryKey]
+    [MapTo("_id")]
+    public ObjectId Id { get; set; } = ObjectId.GenerateNewId();
+
+    [MapTo("user_id")]
+    public string UserId { get; set; }
+
+    [MapTo("high_score")]
+    public int HighScore { get; set; }
+
+    [MapTo("spark_blaster_enabled")]
+    public bool SparkBlasterEnabled { get; set; }
+
+    [MapTo("cross_blaster_enabled")]
+    public bool CrossBlasterEnabled { get; set; }
+
+    public PlayerProfile(string userId) {
+        this.UserId = userId;
+        this.HighScore = 0;
+        this.SparkBlasterEnabled = false;
+        this.CrossBlasterEnabled = false;
+    }
+
+}
